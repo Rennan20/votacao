2609617478a51cd5ec79a6cb74cc5df542c2169b
Rennan Silva <jrennan99@gmail.com>
2023-05-02 12:06:23 -0300

    first commit

---
+++ completo/urna/etapas.js
@@ -0,0 +1,76 @@
+let etapas = [
+  {
+    titulo: "DITADOR",
+    numeros: 5,
+    candidatos: [
+      {
+        numero: "00000",
+        nome: "Cirilo",
+        partido: "PT",
+        fotos: [
+          {
+            url: "1461714_394340070699504_1379885856_n.jpg",
+            legenda: "Ditador",
+          },
+        ],
+      },
+      {
+        numero: "11111",
+        nome: "Danil-o",
+        partido: "PCG",
+        fotos: [{ url: "danil-o.png", legenda: "Ditador" }],
+      },
+      {
+        numero: "22222",
+        nome: "Prata Fraca",
+        partido: "Rock",
+        fotos: [{ url: "prata-fraca.png", legenda: "Ditador" }],
+      },
+      {
+        numero: "33333",
+        nome: " Dantinhas",
+        partido: "ADV",
+        fotos: [{ url: "dantinhas.png", legenda: "Ditador" }],
+      },
+      {
+        numero: "44444",
+        nome: "Matias",
+        partido: "ADV",
+        fotos: [{ url: "matias.png", legenda: "Ditador" }],
+      },
+      {
+        numero: "55555",
+        nome: "Quixas",
+        partido: "MCU",
+        fotos: [{ url: "quixas.png", legenda: "Ditador" }],
+      },
+      {
+        numero: "66666",
+        nome: "Nanner",
+        partido: "DD",
+        fotos: [{ url: "nanner.png", legenda: "Ditador" }],
+      },
+    ],
+  },
+
+  {
+    titulo: "CARRASCO",
+    numeros: 2,
+    candidatos: [
+      {
+        numero: "13",
+        nome: "Lulo",
+        partido: "PT",
+
+        fotos: [{ url: "lulo.png", legenda: "Carrasco" }],
+      },
+      {
+        numero: "17",
+        nome: "Bozo",
+        partido: "PT",
+
+        fotos: [{ url: "bozo.png", legenda: "Carrasco" }],
+      },
+    ],
+  },
+];
